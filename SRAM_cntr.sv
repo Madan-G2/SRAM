a78965fafdfb77fa9c2d9c87ac2eb70c87656042
Vishnu Deep PandurangaRao <143198224+vdspw@users.noreply.github.com>
2025-07-11 09:07:02 -0700

    Update SRAM_cntr.sv
    Logic updated

--- SRAM_cntr.sv
+++ SRAM_cntr.sv
@@ -16,5 +16,74 @@ module sram #( parameter ADDR_WIDTH = 8,      //top-level
     WRITE = 3'b010,
     READ = 3'b100 }state;
   state current_state,next_state;
+
+  logic [DATA_WIDTH-1:0] mem [0:MEM_SIZE-1]; //memory spaces
+  logic [MEM_SIZE-1:0] addr_one_hot; //one hot decdng var
+  always_comb begin
+        addr_one_hot = '0; // initally all are zeros
+        if (!ce_n && addr < MEM_SIZE) begin
+            addr_one_hot[addr] = 1'b1; 
+        end
+    end
+  always_comb begin
+        next_state = current_state;
+        case (current_state)
+            IDLE: begin
+                if (!ce_n && !we_n && re_n) begin
+                    next_state = WRITE;
+                end
+                else if (!ce_n && !re_n && we_n) begin
+                    next_state = READ;
+                end
+            end
+            WRITE: begin
+                if (ce_n || we_n) begin
+                    next_state = IDLE;
+                end
+            end
+            READ: begin
+                if (ce_n || re_n) begin
+                    next_state = IDLE;
+                end
+            end
+            default: next_state = IDLE;
+        endcase
+    end
+
   
+  always @(posedge clk or negedge rst_n) begin //FSM 
+        if (!rst_n) begin
+            current_state <= IDLE;
+        end
+        else begin
+            current_state <= next_state;
+        end
+    end
+
+    
+  always @(posedge clk or negedge rst_n) begin //Synch write
+        if (!rst_n) begin
+            
+            for (int i = 0; i < MEM_SIZE; i++) begin
+                mem[i] <= '0;
+            end
+        end
+        else if (current_state == WRITE && !ce_n && !we_n && addr_one_hot[addr]) begin
+            
+            mem[addr] <= data_in;
+        end
+    end
+
+   
+    always_comb begin //Asych rd operation
+        if (current_state == READ && !ce_n && !re_n && addr_one_hot[addr]) begin
+           
+            data_out = mem[addr];
+        end
+        else begin
+            data_out = '0; 
+        end
+    end
+
+endmodule
 endmodule
